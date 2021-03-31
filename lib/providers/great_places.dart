b6b7984dc138d50796b2dba7526c6e1e1966f366
sara <sara.moura.faria@gmail.com>
2021-03-30 22:35:15 -0300

    add image from camera and save the file

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:great_places/models/place.dart';
 
 class GreatePlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -14,7 +18,16 @@ class GreatePlaces with ChangeNotifier {
   Place itemByIndex(int index) {
     return _items[index];
   }
-}
 
-class Place {
+  void addPlace(String title, File image) {
+    final newPlace = Place(
+      id: Random().nextDouble().toString(), 
+      title: title, 
+      location: null, 
+      image: image,
+    );
+
+    _items.add(newPlace);
+    notifyListeners();
+  }
 }
--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/widgets/image_input.dart';
+import 'package:provider/provider.dart';
 
 class PlaceFormScreen extends StatefulWidget {
   @override
@@ -8,9 +12,21 @@ class PlaceFormScreen extends StatefulWidget {
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
   final _titleController = TextEditingController();
+  File _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
 
   void _submitForm() {
+    if(_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }  
 
+    Provider.of<GreatePlaces>(context, listen: false).addPlace(
+      _titleController.text, _pickedImage);
+
+    Navigator.of(context).pop();
   }
 
   @override
@@ -35,7 +51,7 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
                       ),
                     ),
                     SizedBox(height: 10),
-                    ImageInput(),
+                    ImageInput(this._selectImage),
                   ],
                 ),
               ),
